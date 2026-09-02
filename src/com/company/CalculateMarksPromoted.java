529671d3314035ede2e800df57268e22cd8cc5ed
lavlin kumar <lavishkumar904546@gmail.com>
2026-09-01 19:34:25 -0700

    ForLoopCheckEvenAndOdd

--- src/com/company/CalculateMarksPromoted.java
+++ src/com/company/CalculateMarksPromoted.java
@@ -2,7 +2,7 @@ package com.company;
 
 import java.util.Scanner;
 
-public class PracticeSet_4 {
+public class CalculateMarksPromoted {
      public static void main(String[] args) {
          Scanner sc=new Scanner(System.in);
          byte m1,m2,m3;
---
+++ src/com/company/ForLoopCheckEvenAndOdd.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class ForLoopCheckEvenAndOdd {
+     public static void main(String[] args) {
+         int even=5;
+         for (int i = 0; i <even; i++) {
+             System.out.println(i++);
+         }
+         int  odd=10;
+         for (int i = 0; i<odd ; i++) {
+             System.out.println(2*i+1);
+
+         }
+    }
+}
