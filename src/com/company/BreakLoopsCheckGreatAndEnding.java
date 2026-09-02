fd174b7af80441eca049680fec8c70f0a59b67e4
lavlin kumar <lavishkumar904546@gmail.com>
2026-09-01 19:47:32 -0700

    BreakLoopsCheckGreatAndEnding

---
+++ src/com/company/BreakLoopsCheckGreatAndEnding.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class BreakLoopsCheckGreatAndEnding {
+     public static void main(String[] args) {
+         for (int i = 0; i < 5; i++) {
+             System.out.println(i);
+             System.out.println("java is great");
+             if (i == 2) {
+                 System.out.println("ending the loop");
+                 break;
+
+             }
+
+         }
+
+    }
+}
