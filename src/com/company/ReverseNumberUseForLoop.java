373f607d709637d84e1adc2964096f01323b134e
lavlin kumar <lavishkumar904546@gmail.com>
2026-09-01 19:38:31 -0700

    ReverseNumberUseForLoop

---
+++ src/com/company/ReverseNumberUseForLoop.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class ReverseNumberUseForLoop {
+     public static void main(String[] args) {
+         for (int i =5;i>0;i--) {
+             System.out.println(i);
+
+         }
+
+    }
+}
