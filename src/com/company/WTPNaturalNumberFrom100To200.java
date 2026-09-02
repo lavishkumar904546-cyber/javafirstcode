409acfde3fcc11bf7804a521a4d3969a585c2a07
lavlin kumar <lavishkumar904546@gmail.com>
2026-09-01 20:02:36 -0700

    WTPNaturalNumberFrom100To200

---
+++ src/com/company/WTPNaturalNumberFrom100To200.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class WTPNaturalNumberFrom100To200 {
+     public static void main(String[] args) {
+         int i=100;
+         while (i<=200){
+             System.out.println(i);
+             i++;
+
+         }
+         System.out.println("finish runing whille loops");
+
+    }
+}
