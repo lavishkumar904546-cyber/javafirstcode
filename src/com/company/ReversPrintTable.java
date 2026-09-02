dac37533134ba8575c0dd993a17ac0eb9385e2f4
lavlin kumar <lavishkumar904546@gmail.com>
2026-09-01 20:40:44 -0700

    ReversPrintTable

---
+++ src/com/company/ReversPrintTable.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class  ReversPrintTable{
+     public static void main(String[] args) {
+        int n = 5;
+
+        for (int i = 5; i >= 1; i--) {
+            System.out.printf("%d x %d = %d%n", n, i, n * i);
+        }
+    }
+
+        }
+
+
+
